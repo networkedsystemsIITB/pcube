152fc0080d38d604b8aacf8b636476b3599f5e82
aniket <aniket8897@gmail.com>
2018-06-23 00:07:27 +0530

    Removed copyrights

--- stateful_load_balancer/p4src/generate_p4.py
+++ stateful_load_balancer/p4src/generate_p4.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 
-# Copyright 2018-present Akash Trehan Aniket Shirke
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 ########################################################
 # 1. Convert ip4 format to p4
 ########################################################
@@ -23,7 +9,6 @@ import os
 import re
 import json
 from collections import OrderedDict
-# For recognising the ???
 from pyparsing import Word, alphas, nums, nestedExpr, Keyword, alphanums, Regex, White, Optional
 
 import fileinput
--- stateful_load_balancer/run.sh
+++ stateful_load_balancer/run.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copyright 2013-present Barefoot Networks, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 ###--- Remove stale json
 for j in `seq 1 $2`
 do
@@ -25,7 +11,7 @@ sudo rm -f *.pcap
 sudo mn -c
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source $THIS_DIR/../../../../env.sh
+source $THIS_DIR/../../env.sh
 
 P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
 
