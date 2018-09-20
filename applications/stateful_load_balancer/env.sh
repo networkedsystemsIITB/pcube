f8048ccc5d068cd34e330ffa49fb4e66f49913dd
aniket <aniket8897@gmail.com>
2018-09-21 00:20:40 +0530

    Command files

--- applications/stateful_load_balancer/env.sh
+++ applications/stateful_load_balancer/env.sh
@@ -1,8 +1,8 @@
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # ---------------- EDIT THIS ------------------
-BMV2_PATH=$THIS_DIR/../../bmv2
+BMV2_PATH=$THIS_DIR/../../bmv2_copy
 # e.g. BMV2_PATH=$THIS_DIR/../bmv2
-P4C_BM_PATH=$THIS_DIR/../../p4c-bmv2
+P4C_BM_PATH=$THIS_DIR/../../p4c-bm
 # e.g P4C_BM_PATH=$THIS_DIR/../p4c-bm
 # ---------------- END ------------------
